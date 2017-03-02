71e59204d22a3ad3aa4b2c10c8e9bbae899307e1
Marcus Östling <marcus.ftw94@gmail.com>
2017-03-02 19:07:33 +0100

    :beers: Optional fading added

--- javascript/particle.js
+++ javascript/particle.js
@@ -6,6 +6,7 @@ function Particle(x, y, goalx, goaly){
     var entity = new Entity(x,y);
     var hyp = Math.sqrt(Math.pow(goalx-x,2) + Math.pow(goaly-y,2));
     var alpha = 1;
+    var fading = true;
     entity.speedx = 3;
     entity.speedy = 3;
     
@@ -16,15 +17,20 @@ function Particle(x, y, goalx, goaly){
         entity.angle = -Math.asin((goaly-y)/hyp) + Math.PI;
         
     }
+ 
+    this.setFade = function(bool) {
+        fading = bool;
+    }
     
     entity.update = function() {
         entity.x += Math.cos(entity.angle)*entity.speedx;
         entity.y += Math.sin(entity.angle)*entity.speedy;
 
         // Fade when closer to goal
-        var hyp2 = Math.sqrt(Math.pow(goalx-entity.x,2) + Math.pow(goaly-entity.y,2));
-        alpha = hyp2/hyp;
-
+        if(fading) {
+            var hyp2 = Math.sqrt(Math.pow(goalx-entity.x,2) + Math.pow(goaly-entity.y,2));
+            alpha = hyp2/hyp;
+        }
         // Check if dead
         if(alpha < 0.08) {
             entity.dead = true;
