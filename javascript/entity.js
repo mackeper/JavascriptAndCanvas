5981085e5e4ccab984b67c6bb0eb4d0e81c8a6d4
Marcus Östling <marcus.ftw94@gmail.com>
2017-03-02 21:42:34 +0100

    :zap: Uses a size variable

--- javascript/entity.js
+++ javascript/entity.js
@@ -10,14 +10,21 @@ function Entity(x,y) {
     this.angle = 0;
     this.dead = false;
     this.color = "#000000";
+    var size = 3;
+    
+    this.setSize = function(newSize) {
+        size = newSize;
+    }
+
     this.update = function() {
         this.x += Math.cos(this.angle) * this.speedx;
         this.y += Math.sin(this.angle) * this.speedy;
     }
+
     this.render = function(ctx) {
         ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.arc(this.x,this.y,5,0,2*Math.PI);
+        ctx.arc(this.x,this.y,size,0,2*Math.PI);
         ctx.fill();
     }
 }
