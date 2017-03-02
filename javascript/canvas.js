9555b7fbaa3500892cd9ed144d5ec9c205271582
Marcus Östling <marcus.ftw94@gmail.com>
2017-03-02 14:32:37 +0100

    :zap: Now renders a list of entities

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -2,33 +2,34 @@
 Canvas class
 */
 function Canvas(width, height) {
-this.width = width;
-this.height = height;
-this.canvas = document.createElement('canvas');
-this.canvas.height = this.height;
-this.canvas.width = this.width;
+    this.width = width;
+    this.height = height;
+    this.canvas = document.createElement('canvas');
+    this.canvas.height = this.height;
+    this.canvas.width = this.width;
 }
 
 /**
 function that starts the loop
 */
 function run() {
-var canvasObject = new Canvas(1280,720);
-document.body.appendChild(canvasObject.canvas);
+    var canvasObject = new Canvas(1280,720);
+    document.body.appendChild(canvasObject.canvas);
 
-var time = 0;
-loop(canvasObject, time);
+    var entityList = [new Entity(100,100)];
 
+    var time = 0;
+    loop(canvasObject, entityList, time);
 }
-
 /**
 Game/program loop
 */
-function loop(canvasObject, time){
+
+function loop(canvasObject, entityList, time){
 setTimeout(function () {
-update(time);
-render(canvasObject);
-loop()
+    //update(time);
+    render(canvasObject, entityList);
+    loop(canvasObject, entityList, time);
 }, 16);
 };
 
@@ -42,9 +43,14 @@ function update(time){
 /**
 render all entites
 */
-function render(canvasObject){
-var ctx = canvasObject.canvas.getContext("2d");
-ctx.fillStyle = 'rgb(230,230,255)';
-ctx.fillRect(0,0,canvasObject.width, canvasObject.height)
+function render(canvasObject, entityList){
+    var ctx = canvasObject.canvas.getContext("2d");
+    //Background
+    ctx.fillStyle = 'rgb(230,230,255)';
+    ctx.fillRect(0,0,canvasObject.width, canvasObject.height)
 
+    //Entities
+    for(var i = 0; i < entityList.length; i++) {
+        entityList[i].render(ctx);
+    }
 }
