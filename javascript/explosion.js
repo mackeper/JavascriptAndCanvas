2e4f0ec929fd35972e0955b5003bea6155ede58b
Marcus Östling <marcus.ftw94@gmail.com>
2017-03-02 21:42:02 +0100

    :beers: Explosion class

---
+++ javascript/explosion.js
@@ -0,0 +1,38 @@
+/**
+   Explosion class
+*/
+function Explosion(x,y,size) {
+    var entity = new Entity(x,y);
+    var amount = size/5;
+    var particleList = [];
+    
+    for(var i = 0; i < amount; i++) {
+        var angle = Math.random()*Math.PI*2;
+        var radius = size + (Math.random()*size/6)*
+            (Math.round(Math.random())*2-1);
+        var goalx = radius * Math.cos(angle) + x;
+        var goaly = radius * Math.sin(angle) + y;
+        var p = new Particle(x,y,goalx,goaly);
+        var speed = Math.random()*3 + 3;
+        p.speedy = speed;
+        p.speedx = speed;
+        particleList.push(p);
+    }
+
+    entity.update = function() {
+        for(var i = 0; i < particleList.length; i++) {
+            particleList[i].update();
+            if(particleList[i].dead == true) {
+                particleList.splice(i,1);
+            }
+        }
+    }
+
+    entity.render = function(ctx) {
+        for(var i = 0; i < particleList.length; i++) {
+            particleList[i].render(ctx);
+        }
+    }
+
+    return entity;
+}
