8c031e3c6da2594148580cb1a3810cfff25e71d3
Marcus Östling <marcus.ftw94@gmail.com>
2017-03-03 16:48:58 +0100

    :zap: Renders a label

--- javascript/hud/hud.js
+++ javascript/hud/hud.js
@@ -1,3 +1,15 @@
-function Hud(canvasObject) {
+function Hud() {
+    var entity = new Entity(0,0);
+    var elementList = [];
     
+    //Add a label
+    elementList.push(new HudLabel(10,10,"Super mega fun canvas", 30));
+    
+    entity.render = function(ctx) {
+        for(var i = 0; i < elementList.length; i++) {
+            elementList[i].render(ctx);
+        }
+    }
+    
+    return entity;
 }
