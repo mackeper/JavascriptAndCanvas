fe5a55f727f914b6aad6a4d150edfd5b10fe0e77
Marcus Östling <marcus.ftw94@gmail.com>
2017-03-03 19:58:48 +0100

    :zap: now has 2 working buttons

--- javascript/hud/hud.js
+++ javascript/hud/hud.js
@@ -10,13 +10,23 @@ function Hud(canvasObject) {
     elementList.push(new HudLabel(10,10,"Super mega fun canvas", 30));
     
     //Add a button
-    var b1 = new HudButton(100,100,200,200, function() {
+    var b1 = new HudButton(10,80,40,40, function() {
         canvasObject.getTool = function(x,y) {
-            return new Starfield(x-250,y-250,x+250,y+250,x,y);
+            var sf = new Starfield(x-250,y-250,x+250,y+250,x,y);
+            sf.setTime(5000);
+            return sf;
         }
     });
-    b1.setText("Starfield",30);
+    b1.setText("Starfield",10);
     elementList.push(b1);
+    
+    var b2 = new HudButton(10, 130, 40, 40, function() {
+       canvasObject.getTool = function(x,y) {
+           return new Explosion(x,y, Math.random()*500+50);
+       } 
+    });
+    b2.setText("Explosion",10);
+    elementList.push(b2);
 
     entity.render = function(ctx) {
         for(var i = 0; i < elementList.length; i++) {
