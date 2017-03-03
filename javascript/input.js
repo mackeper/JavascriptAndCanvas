91c02bce855bd7739dc82981cb566bd8a157850a
Marcus Östling <marcus.ftw94@gmail.com>
2017-03-03 18:17:45 +0100

    :zap: Handles hud and different mouse actions

--- javascript/input.js
+++ javascript/input.js
@@ -2,7 +2,6 @@
    Input class
    Handling all user input
  */
-
 function Input(canvasObject, hudObject) {
     var canvas = $('#mainCanvas');
     var canvasPosition = {
@@ -10,14 +9,48 @@ function Input(canvasObject, hudObject) {
         y: canvas.offset().top
     };
     
-    canvas.on('click', function(e) {
+    function getMouse(e) {
         e .preventDefault();
 
         var mouse = {
             x: e.pageX - canvasPosition.x,
             y: e.pageY - canvasPosition.y
         };
-        
+        return mouse;
+    }
+
+    var mouseActions = {
+        CLICK : 0,
+        DOWN : 1
+    };
+    
+    function checkHud(mouse, action) {
+        var elementList = hudObject.getHudElementList();
+        for(var i = 0; i < elementList.length; i++) {
+            var e = elementList[i];
+            if(e.intersect(mouse.x,mouse.y)) {
+                switch(action) {
+                    case mouseActions.CLICK:
+                    e.onClick();
+                    break;
+                    case mouseActions.DOWN:
+                    e.onMouseDown();
+                    break;
+                }
+            }
+        }
+    }
+    
+    canvas.on('click', function(e) {
+        var mouse = getMouse(e);
+        checkHud(mouse, mouseActions.CLICK);
+        canvasObject.pushEntity(
+            new Explosion(mouse.x, mouse.y, Math.random()*500+50 ));
+   });
+    
+   canvas.on('mousedown', function(e) {
+        var mouse = getMouse(e);
+        checkHud(mouse, mouseActions.DOWN);
         canvasObject.pushEntity(
             new Explosion(mouse.x, mouse.y, Math.random()*500+50 ));
    });
