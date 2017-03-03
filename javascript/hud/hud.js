49f733cca5b25a0fd21dbca56ead6c6577460522
Marcus Östling <marcus.ftw94@gmail.com>
2017-03-03 18:16:27 +0100

    :zap: button and getHudElementList

--- javascript/hud/hud.js
+++ javascript/hud/hud.js
@@ -2,9 +2,18 @@ function Hud() {
     var entity = new Entity(0,0);
     var elementList = [];
     
+    entity.getHudElementList = function() {
+        return elementList;
+    }
+
     //Add a label
     elementList.push(new HudLabel(10,10,"Super mega fun canvas", 30));
     
+    //Add a button
+    elementList.push(new HudButton(100,100,200,200, function() {
+        // do nothing
+    }));
+
     entity.render = function(ctx) {
         for(var i = 0; i < elementList.length; i++) {
             elementList[i].render(ctx);
