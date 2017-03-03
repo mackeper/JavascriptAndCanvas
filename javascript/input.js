35bde966698a241504526565de124ff5893b914d
Marcus Östling <marcus.ftw94@gmail.com>
2017-03-03 01:02:17 +0100

    :tada: Class to handle user input

---
+++ javascript/input.js
@@ -0,0 +1,24 @@
+/**
+   Input class
+   Handling all user input
+ */
+
+function Input(canvasObject) {
+    var canvas = $('#mainCanvas');
+    var canvasPosition = {
+        x: canvas.offset().left,
+        y: canvas.offset().top
+    };
+    
+    canvas.on('click', function(e) {
+        e .preventDefault();
+
+        var mouse = {
+            x: e.pageX - canvasPosition.x,
+            y: e.pageY - canvasPosition.y
+        };
+        
+        canvasObject.pushEntity(
+            new Explosion(mouse.x, mouse.y, Math.random()*500+50 ));
+   });
+}
