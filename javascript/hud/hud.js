1c28244c03e76500a5e9d4a169f8fab5fb389a90
Marcus Östling <marcus.ftw94@gmail.com>
2017-03-03 19:03:58 +0100

    :zap: Take canvasObject in constructor

--- javascript/hud/hud.js
+++ javascript/hud/hud.js
@@ -1,4 +1,4 @@
-function Hud() {
+function Hud(canvasObject) {
     var entity = new Entity(0,0);
     var elementList = [];
     
@@ -10,9 +10,13 @@ function Hud() {
     elementList.push(new HudLabel(10,10,"Super mega fun canvas", 30));
     
     //Add a button
-    elementList.push(new HudButton(100,100,200,200, function() {
-        // do nothing
-    }));
+    var b1 = new HudButton(100,100,200,200, function() {
+        canvasObject.getTool = function(x,y) {
+            return new Starfield(x-250,y-250,x+250,y+250,x,y);
+        }
+    });
+    b1.setText("Starfield",30);
+    elementList.push(b1);
 
     entity.render = function(ctx) {
         for(var i = 0; i < elementList.length; i++) {
