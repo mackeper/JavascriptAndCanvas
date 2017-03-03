f3825a6bf5842be02ccfa546f8682e0f6900784b
Marcus Östling <marcus.ftw94@gmail.com>
2017-03-03 19:25:59 +0100

    :beers: Added debug mode

---
+++ javascript/debug/debug.js
@@ -0,0 +1,15 @@
+var debugMode = false;
+var debugDiv = document.createElement('div');
+
+function setDebugMode(bool) {
+    debugMode = bool;
+    document.body.appendChild(debugDiv);
+}
+
+function setDebugText(str) {
+    if(!debugMode) {
+       return;
+    }
+    
+    debugDiv.innerHTML = str;
+}
